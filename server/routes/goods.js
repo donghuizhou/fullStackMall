3eff197b659ad6aa50a85ef28bc7b7b2aec3420e
donghuizhou <765966797@qq.com>
2017-12-11 17:47:36 +0800

    价格筛选

--- server/routes/goods.js
+++ server/routes/goods.js
@@ -18,30 +18,94 @@ mongoose.connection.on('disconnected', () => {
   console.log('MongoDB connected disconnected')
 })
 
+// 查询商品列表数据
 router.get('/', (req, res, next) => {
   let page = parseInt(req.param('page'))
   let pageSize = parseInt(req.param('pageSize'))
+  let priceLevel = req.param('priceLevel')
   let sort = req.param('sort')
   let skip = (page - 1) * pageSize
+  let priceGt = '', priceLte = ''
   let params = {}
-
+  if (priceLevel !== 'all') {
+    switch (priceLevel) {
+      case '0': priceGt = 0; priceLte = 100; break;
+      case '1': priceGt = 100; priceLte = 500; break;
+      case '2': priceGt = 500; priceLte = 1000; break;
+      case '3': priceGt = 1000; priceLte = 5000; break;
+    }
+    params = {
+      salePrice: {
+        $gt: priceGt,
+        $lte: priceLte
+      }
+    }
+  }
   let goodsModel = Goods.find(params).skip(skip).limit(pageSize)
   goodsModel.sort({'salePrice': sort})
   goodsModel.exec((err, doc) => {
     if (err) {
       res.json({
-        status: 501,
+        code: 501,
         msg: err.message
       })
     } else {
       res.json({
-        status: 200,
+        code: 200,
         msg: '获取信息成功',
         result: {
           count: doc.length,
           list: doc
         }
       })
+    }
+  })
+})
+
+// 加入到购物车
+router.post('/goods/addCart', (req, res, next) => {
+  let userId = '100000077', 
+      productId = req.body.productId
+  let User = require('./../models/user.js')      // 获取模型
+  
+  User.findOne({userId: userId}, (err1, userDoc) => {
+    if (err1) {
+      res.json({
+        code: 501,
+        msg: err1.msg
+      })
+    } else {
+      console.log('userDoc:' + userDoc)
+      if (userDoc) {
+        Goods.findOne({productId: productId}, (err2, doc2) => {
+          if (err2) {
+            res.json({
+              code: 501,
+              msg: err2.msg
+            })
+          } else {
+            if (doc2) {
+              doc.productNum = 1
+              doc.checked = 1
+              User.cartList.push(doc2)
+              User.save((err3, doc3) => {
+                if (err3) {
+                  res.json({
+                    code: 501,
+                    msg: err3.msg
+                  })
+                } else {
+                  res.json({
+                    code: 200,
+                    msg: '添加成功',
+                    result: 'success'
+                  })
+                }
+              })
+            }
+          }
+        })
+      }
     }
   })
 })
--- src/views/goodsList.vue
+++ src/views/goodsList.vue
@@ -9,7 +9,12 @@
         <div class="filter-nav">
           <span class="sortby">Sort by:</span>
           <a href="javascript:void(0)" class="default cur">Default</a>
-          <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
+          <a href="javascript:void(0)" @click="sortGoods" class="price">
+            Price 
+            <svg class="icon icon-arrow-short">
+              <use xlink:href="#icon-arrow-short"></use>
+            </svg>
+          </a>
           <a href="javascript:void(0)" class="filterby stopPop" @click="showFilterPop">Filter by</a>
         </div>
         <div class="accessory-result">
@@ -28,7 +33,7 @@
           <div class="accessory-list-wrap">
             <div class="accessory-list col-4">
               <ul>
-                <li v-for="item in goodsList.list" :key="item.productId">
+                <li v-for="item in goodsList" :key="item.productId">
                   <div class="pic">
                     <a href="#">
                       <img v-lazy="'/static/' + item.productImage" alt="">
@@ -36,13 +41,18 @@
                   </div>
                   <div class="main">
                     <div class="name">{{item.productName}}</div>
-                    <div class="price">{{item.productPrice}}</div>
+                    <div class="price">{{item.salePrice}}</div>
                     <div class="btn-area">
-                      <a href="javascript:;" class="btn btn--m">加入购物车</a>
+                      <a @click="addCart(item.produceId)" href="javascript:;" class="btn btn--m">加入购物车</a>
                     </div>
                   </div>
                 </li>
               </ul>
+              <!-- loading -->
+              <div v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="20" class="load-more">
+                <img src="/static/loading/loading-bubbles.svg" v-if="loading">
+              </div>
+
             </div>
           </div>
         </div>
@@ -70,6 +80,10 @@ export default {
       priceFilter: [
         {
           startPrice: '0.00',
+          endPrice: '100.00'
+        },
+        {
+          startPrice: '100.00',
           endPrice: '500.00'
         },
         {
@@ -78,21 +92,62 @@ export default {
         },
         {
           startPrice: '1000.00',
-          endPrice: '2000.00'
+          endPrice: '5000.00'
         }
       ],
       priceChecked: 'all',
       filterBy: false,
-      overLayFlag: false
+      overLayFlag: false,
+      sortFlag: true,
+      page: 1,
+      pageSize: 8,
+      busy: false,
+      loading: false
     }
   },
   methods: {
-    getGoodsList () {
-      axios.get('/goods')
-        .then(res => {
-          let result = res.data
-          this.goodsList = result.result
+    getGoodsList (flag) {
+      this.loading = true
+      let param = {
+        page: this.page,
+        pageSize: this.pageSize,
+        sort: this.sortFlag ? 1 : -1,
+        priceLevel: this.priceChecked
+      }
+      axios.get('/goods', {
+        params: param
+        }).then(res => {
+          res = res.data
+          if (res.code === 200) {
+            if (flag) {
+              this.goodsList = this.goodsList.concat(res.result.list)
+
+              if (res.result.count === 0) {
+                this.busy = true
+              } else {
+                this.busy = false
+              }
+            } else {
+              this.goodsList = res.result.list
+              this.busy = false
+            }
+          } else {
+            this.goodsList = []
+          }
+          this.loading = false
         })
+    },
+    sortGoods () {
+      this.sortFlag = !this.sortFlag
+      this.page = 1
+      this.getGoodsList()
+    },
+    loadMore () {
+      this.busy = true
+      setTimeout(() => {
+        this.page++
+        this.getGoodsList(true)
+      }, 500)
     },
     showFilterPop () {
       this.filterBy = true
@@ -104,7 +159,18 @@ export default {
     },
     setPriceFilter (index) {
       this.priceChecked = index
+      this.page = 1
+      this.getGoodsList()
       this.closePop()
+    },
+    addCart (productId) {
+      axios.post('/goods/addCart', {
+        productId
+      }).then(res => {
+        if (res.data.code === 200) {
+          console.log()
+        }
+      })
     }
   },
   mounted () {
@@ -119,4 +185,8 @@ export default {
 </script>
 
 <style lang='stylus' scoped rel='stylesheet/stylus'>
+.load-more 
+  height 100px
+  line-height 100px
+  text-align center
 </style>
